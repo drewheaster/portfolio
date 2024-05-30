cc4d7e7a79c63faaddae45c3fce000ca0a9bf1d1
Drew Easter <drewheaster@gmail.com>
2024-05-29 21:49:31 -0500

    added background gradient and set the header to be on the side

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,10 +36,10 @@ export default function Header(props) {
 
     return (
         <header className="header">
-            <div onClick={ event => window.location.href ='/'} className= { HomeUnderlined ? "page-selection-underlined" : "page-selection"}>Home</div>
-            <div onClick={ event => window.location.href ='/aboutme'} className= { AboutMeUnderlined ? "page-selection-underlined" : "page-selection"}>About Me</div>
-            <div onClick={ event => window.location.href ='/projects'} className= { ProjectsUnderlined ? "page-selection-underlined" : "page-selection"}>Projects</div>
-            <div onClick={ event => window.location.href ='/contact'} className= { ContactUnderlined ? "page-selection-underlined" : "page-selection"}>Contact</div>
+            <div onClick={ event => window.location.href ='/' } className= { HomeUnderlined ? "page-selection-underlined" : "page-selection" }>Home</div>
+            <div onClick={ event => window.location.href ='/aboutme' } className= { AboutMeUnderlined ? "page-selection-underlined" : "page-selection" }>About Me</div>
+            <div onClick={ event => window.location.href ='/projects' } className= { ProjectsUnderlined ? "page-selection-underlined" : "page-selection" }>Projects</div>
+            <div onClick={ event => window.location.href ='/contact' } className= { ContactUnderlined ? "page-selection-underlined" : "page-selection" }>Contact</div>
             <div className= "page-selection">My Resume</div>
         </header>
     );
