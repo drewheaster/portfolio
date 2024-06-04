23743e129d1790cb97e2c8febeff8614d158114c
Drew Easter <drewheaster@gmail.com>
2024-06-04 11:47:58 -0500

    added more styling to homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -10,7 +10,7 @@ export default function HomePage(props) {
                     <div className="home-title-container">
                         <h1 className="home-title">Drew Easter</h1>
                         <p className="home-desc">
-                            <span className="carrot">&lt;</span><span className="h2-tag">h2</span> <span className="class-tag">className</span>=<span className="class-prop">'my-title'</span><span className="carrot">&gt;</span><br/><span id='title'>Front-End Web Developer</span><br/><span className="carrot">&lt;/</span><span className="h2-tag">h2</span><span className="carrot">&gt;</span>
+                            <span className="carrot">&lt;</span><span className="h2-tag">h2</span> <span className="class-tag">className</span>=<span className="class-prop">'my-title'</span><span className="carrot">&gt;</span><br/><p className='title'>Front-End Web Developer </p><span className="carrot">&lt;/</span><span className="h2-tag">h2</span><span className="carrot">&gt;</span>
                         </p>
                     </div>
                     <img className="home-profile-img" src={ProfileImg} alt="young man playing an orange guitar"></img>
