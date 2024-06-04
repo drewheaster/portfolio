d18e9fe1d91ccd3b1fd6877b56c66738f2020ce1
Drew Easter <drewheaster@gmail.com>
2024-06-04 11:05:18 -0500

    adjusted responsiveness for mobile site in HomePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -9,7 +9,9 @@ export default function HomePage(props) {
                 <div className="home-main-container">
                     <div className="home-title-container">
                         <h1 className="home-title">Drew Easter</h1>
-                        <p className="home-desc"><span className="carrot">&lt;</span><span className="p-tag">p</span> <span className="class-tag">className</span>=<span className="class-prop">'my-title'</span><span className="carrot">&gt;</span> <span id='title'>Front-End Web Developer</span> <span className="carrot">&lt;/</span><span className="p-tag">p</span><span className="carrot">&gt;</span></p>
+                        <p className="home-desc">
+                            <span className="carrot">&lt;</span><span className="h2-tag">h2</span> <span className="class-tag">className</span>=<span className="class-prop">'my-title'</span><span className="carrot">&gt;</span><br/><span id='title'>Front-End Web Developer</span><br/><span className="carrot">&lt;/</span><span className="h2-tag">h2</span><span className="carrot">&gt;</span>
+                        </p>
                     </div>
                     <img className="home-profile-img" src={ProfileImg} alt="young man playing an orange guitar"></img>
                 </div>
