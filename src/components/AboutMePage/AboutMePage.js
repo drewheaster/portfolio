2944b13352b26e9bd6240a7c48e709faf5a460e3
Drew Easter <drewheaster@gmail.com>
2024-10-30 18:35:20 -0500

    created build

--- src/components/AboutMePage/AboutMePage.js
+++ src/components/AboutMePage/AboutMePage.js
@@ -38,7 +38,30 @@ export default function AboutMePage(props) {
         <>
             <div className="page-bg">
                 <div className="page-main-container" id="about-page-container">
-
+                    <div class="about-me-info-container">
+                        <div class="skills-bar">
+                            <i class="fa-brands fa-html5"></i>
+                            <i class="fa-brands fa-css3-alt"></i>
+                            <i class="fa-brands fa-js"></i>
+                            <i class="fa-brands fa-node-js"></i>
+                            <i class="fa-brands fa-react"></i>
+                            <i class="fa-brands fa-python"></i>
+                        </div>
+                        <div class="about-me-desc">
+                            <div>
+                                <h2 class="desc-title">Where I've Been -</h2>
+                                <p class="">I started learning about software development in May of 2023. I didn't have a technical background, so when I learned what was possible through code even at the basic level, I was hooked.</p>
+                            </div>
+                            <div>
+                                <h2 class="desc-title">Where I Am -</h2>
+                                <p class="">Since I began my journey with Python scripts, I've developed the desire to learn more about web development. Currently, I'm taking a course on full-stack development through SMU in Dallas, TX. I love working in React, creating webpages, and writing clean code!</p>
+                            </div>
+                            <div>
+                                <h2 class="desc-title">Where I'm Going -</h2>
+                                <p class="">I look forward to completing my classes and taking on a Front-End developer or a Full-Stack developer role full-time. I'm interested in the startup space, as well as creating clean code for anyone looking to hire!</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
